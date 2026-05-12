93f0e031815f9f192a4b49db888e0bda73f8a2f3
verekia <522007+verekia@users.noreply.github.com>
2026-05-12 15:33:27 +0700

    perf: gzip image

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker buildx build --platform linux/arm64 --load -t verekia/nanothree .
-docker save -o /tmp/nanothree.tar verekia/nanothree
-scp /tmp/nanothree.tar midgar:/tmp/
-ssh midgar docker load --input /tmp/nanothree.tar
+docker save verekia/nanothree | gzip > /tmp/nanothree.tar.gz
+scp /tmp/nanothree.tar.gz midgar:/tmp/
+ssh midgar docker load --input /tmp/nanothree.tar.gz
 ssh midgar docker compose up -d nanothree
